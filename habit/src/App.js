e69c558fcceb0a9710d02c5a2941287b4a52dbc6
Sonia Li <sonialxy00@gmail.com>
2023-01-23 22:00:06 -0500

    Navbar (doesnt show text)

--- habit/src/App.js
+++ habit/src/App.js
@@ -1,13 +1,35 @@
-
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/index.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about.js';
+import Events from './pages/events.js';
+import AnnualReport from './pages/annual';
+import Teams from './pages/team';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    return (
+        <div>
 
-    </div>
-  );
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path='/' exact component={Home()} />
+                    <Route path='/about' component={About()} />
+                    <Route path='/events' component={Events()} />
+                    <Route path='/annual' component={AnnualReport()} />
+                    <Route path='/team' component={Teams} />
+                    <Route path='/blogs' component={Blogs} />
+                    <Route path='/sign-up' component={SignUp} />
+                </Routes>
+            </BrowserRouter>
+
+        </div>
+
+    );
 }
 
 export default App;
